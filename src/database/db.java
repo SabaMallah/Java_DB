27588a4591d14b5bb386c0704921a6e3f11d90b0
Saba Mallah <mallahsaba28@gmail.com>
2025-01-28 10:03:29 +0500

    INSERT Query

--- src/database/db.java
+++ src/database/db.java
@@ -6,28 +6,37 @@ public class db {
     public static void main(String[] args) throws SQLException {
         String url = "jdbc:sqlserver://HP2020;databaseName=SabaDB;integratedSecurity= true;encrypt=false";
         String query = "SELECT * FROM Students";
+        String insertQuery = "INSERT INTO Students(sname , city) VALUES (?,?)";
 
         System.out.println(url);
-        try{
-            try(Connection connection = DriverManager.getConnection(url)){
-                System.out.println("connection established");
+        try {
+            try(Connection connection = DriverManager.getConnection(url)) {
+                try (PreparedStatement preparedStatement = connection.prepareStatement(insertQuery)) {
 
-                Statement statement = connection.createStatement();
-                ResultSet resultset = statement.executeQuery(query);
+                    preparedStatement.setString(1, "Haya"); // Set the name
+                    preparedStatement.setString(2, "Karachi"); // Set the city
 
-                while (resultset.next()){
-                    String name = resultset.getString("sname");
-                    String city = resultset.getString("city");
-
-                    System.out.println(" Name: " +name+" | "+" City: " + city);
+                    int rowsInserted = preparedStatement.executeUpdate();
+                    if (rowsInserted > 0) {
+                        System.out.println("A new student was inserted successfully!");
+                    }
                 }
 
-            }
+                Statement statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery(query);
 
+                while (resultSet.next()) {
+                    String name = resultSet.getString("sname");
+                    String city = resultSet.getString("city");
+
+                    System.out.println(" Name: " + name + " | " + " City: " + city);
+                }
+            }
         }catch (SQLException e){
             System.out.println("error");
             e.printStackTrace();
         }
+
 
 
     }
