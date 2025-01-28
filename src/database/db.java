f7542746f2ad1d3d70c46801de1cbcb0d182c4d1
Saba Mallah <mallahsaba28@gmail.com>
2025-01-28 10:44:37 +0500

    UPDATE  Query

--- src/database/db.java
+++ src/database/db.java
@@ -6,19 +6,21 @@ public class db {
     public static void main(String[] args) throws SQLException {
         String url = "jdbc:sqlserver://HP2020;databaseName=SabaDB;integratedSecurity= true;encrypt=false";
         String query = "SELECT * FROM Students";
-        String insertQuery = "INSERT INTO Students(sname , city) VALUES (?,?)";
+//        String insertQuery = "INSERT INTO Students(sname , city) VALUES (?,?)";
+        String updateQuery = "UPDATE Students SET sname = ? WHERE city =  ?";
 
         System.out.println(url);
         try {
             try(Connection connection = DriverManager.getConnection(url)) {
-                try (PreparedStatement preparedStatement = connection.prepareStatement(insertQuery)) {
+                try (PreparedStatement preparedStatement = connection.prepareStatement(updateQuery)) {
 
-                    preparedStatement.setString(1, "Haya"); // Set the name
-                    preparedStatement.setString(2, "Karachi"); // Set the city
+                    preparedStatement.setString(1, "Missy");
+                    preparedStatement.setString(2, "US");
 
-                    int rowsInserted = preparedStatement.executeUpdate();
-                    if (rowsInserted > 0) {
-                        System.out.println("A new student was inserted successfully!");
+
+                    int rowsUpdated= preparedStatement.executeUpdate();
+                    if (rowsUpdated > 0) {
+                        System.out.println("A student was updated successfully!");
                     }
                 }
 
